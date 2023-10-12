360082e4dcd6edbba347b31efaec3ee7e66a7607
Shin_MAC <shinss305@gmail.com>
2023-10-13 01:50:39 +0900

    앱 폴더링

--- app/app.js
+++ app/app.js
@@ -1,18 +1,17 @@
 "use strict";
 
-
-
 // 모듈 
 const express = require("express");
 const app = express();
 
 // 라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home);
+app.use(express.static(`${__dirname}/src/public`));
 
 module.exports = app;
